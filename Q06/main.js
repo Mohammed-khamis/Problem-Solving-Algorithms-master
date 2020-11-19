9f306da0c6c54cc1bd873cda3ae9d45d3bdc17cd
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-11-19 14:52:20 +0200

    q6 solution

--- Q06/main.js
+++ Q06/main.js
@@ -7,7 +7,23 @@ Write a function that returns true if a number is even, and false otherwise.
 */
 
 function evenOrNot() {
-  // YOUR CODE HERE
+  if (arguments[0] % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+/* Q7: Sum Array Odd */
+function SumOddNumber() {
+	const oddNumber = arguments[0].filter((arg) => {
+		return arg % 2 !== 0;
+	});
+	const result = oddNumber.reduce((sum, num) => {
+		return sum + num;
+	}, 0);
+
+	return result;
 }
 
 /* 
