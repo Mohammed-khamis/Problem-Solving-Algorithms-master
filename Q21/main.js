4f60f84c60b6d4978cb04623d960d7058aa00f9e
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-12-09 09:12:51 +0200

    Q21 solution

--- Q21/main.js
+++ Q21/main.js
@@ -6,9 +6,20 @@ console.log('Problem Solving Q: 21');
 Given a string, remove any characters that are not unique from the string.
 */
 
-function onlyUnique() {
-  // YOUR CODE HERE
+function onlyUnique(str) {
+	const arr = str.split('');
+	const uniqueArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])) {
+			uniqueArr.push(arr[i]);
+		}
+	}
+	return uniqueArr.join('');
 }
+
+console.log(onlyUnique('hello there'));
+console.log(onlyUnique('xyz'));
+console.log(onlyUnique('iiii'));
 
 /* 
 Examples:
