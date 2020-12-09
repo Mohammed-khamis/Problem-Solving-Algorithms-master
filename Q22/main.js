61ea8a0b05a03502d9ea8244f25202ebdd0b934b
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-12-09 09:56:15 +0200

    Q22 solution

--- Q22/main.js
+++ Q22/main.js
@@ -8,9 +8,27 @@ console.log('Problem Solving Q: 22');
  ascending order by character.
 */
 
-function characterFrequency() {
-  // YOUR CODE HERE
+function characterFrequency(str) {
+	const arr = str.split('').sort();
+	const arrChar = [];
+	const arrLength = [];
+	const arrOrder = [];
+	for (let i = 0; i < arr.length; i++) {
+		const arrOfFrequencyChar = arr.filter((char) => char === arr[i]);
+		if (!arrChar.find((item) => item[0] === arrOfFrequencyChar[0])) {
+			arrChar.push(arrOfFrequencyChar[0]);
+			arrLength.push(arrOfFrequencyChar.length);
+		}
+	}
+	for (let j = 0; j < arrLength.length; j++) {
+		const arr2 = [arrChar[j], arrLength[j]];
+		arrOrder.push(arr2);
+	}
+	return arrOrder.sort((item1, item2) => item2[1] - item1[1]);
 }
+
+console.log(characterFrequency('mississippi'));
+console.log(characterFrequency('miaaiaaippi'));
 
 /* 
 Examples:
