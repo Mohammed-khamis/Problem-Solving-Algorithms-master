458267c86eca3bdddce5214c5e0e432b1b05bd69
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-12-10 06:22:31 +0200

    Q23 solution

--- Q23/main.js
+++ Q23/main.js
@@ -7,10 +7,37 @@ Given a properly formatted URL, return the query string data as an array of key-
 If the url does not contain a query string, return undefined
 */
 
-function parseQueryString() {
-  // YOUR CODE HERE
+function parseQueryString(str) {
+	const paramsArr = [];
+	const query = [];
+	if (str.indexOf('&') !== -1 && str.indexOf('?') !== -1) {
+		const params = str.split('?')[1].split('&');
+		for (let i = 0; i < params.length; i++) {
+			params[i] = params[i].split('=');
+			if (params[i][1].indexOf('%20') !== -1) {
+				params[i][1] = params[i][1].split('%20').join(' ');
+			}
+		}
+		query.push(params);
+		return query;
+	} else if (str.indexOf('?') !== -1) {
+		const params = str.split('?')[1].split('=');
+		if (params[1].indexOf('%20') !== -1) {
+			params[1] = params[1].split('%20').join(' ');
+			query.push(params);
+		} else {
+			query.push(params);
+		}
+
+		return query;
+	} else {
+		return undefined;
+	}
 }
 
+console.log(parseQueryString('http://example.com?a=hello&b=99'));
+console.log(parseQueryString('http://example.com?msg=hello%20world'));
+console.log(parseQueryString('http://example.com'));
 
 /* 
 Examples:
